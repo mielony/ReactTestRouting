3d848471aae22940f5a144a901774f401daa5d44
mielony <mielniczuk.mariusz@gmail.com>
2022-02-13 23:41:20 +0100

    Menu done, Header done.

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import '../styles/Header.css';
+import { Route, Routes } from "react-router-dom";
+
+import img1 from '../images/1.png';
+import img2 from '../images/2.png';
+import img3 from '../images/3.png';
+import img4 from '../images/4.png';
 
 function Header() {
-    return (<div>Header</div>);
+    return (
+        <Routes>
+            <Route path="/" element={ <img src={img1} alt="BMW F31" /> }/> 
+            <Route path="/adages" element={ <img src={img2} alt="CLA" /> }/> 
+            <Route path="/contact" element={ <img src={img3} alt="Superb" /> }/> 
+            <Route path="/admin"  element={ <img src={img4} alt="BMW F34" /> }/> 
+        </Routes>
+    );
 }
 
 export default Header;
--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import '../styles/navigation.css';
+import { NavLink } from "react-router-dom";
+
+
+const menu = [
+    {name: "Home", path: "/"},
+    {name: "Adages", path: "/adages"},
+    {name: "Contact", path: "/contact"},
+    {name: "Admin", path: "/admin"},
+    //{name: "bla", path:"/bla"},
+]
 
 function Navigation() {
-    return (<div>Navigation</div>);
+    const menuitems = menu.map( menuitem => (
+        <li key={menuitem.name} >
+            <NavLink to={menuitem.path} >{menuitem.name} </NavLink>
+        </li>
+    ))
+    return (
+        <nav className="main">
+            <ul>
+                {menuitems}
+            </ul>
+        </nav>
+    )
 }
 
 export default Navigation;
